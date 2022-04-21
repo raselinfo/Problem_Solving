d14b68d9f5a61ad7c3984ba97bf5f861082712ae
John Doe <rabinarahman@gmail.com>
2022-04-21 11:37:11 -0700

    Sum of array element

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function solveMeFirst(a, b) {
-    let result = a + b
-    return result
+function simpleArraySum(ar) {
+    // Write your code here
+    let n = ar.length
+    let sum = 0
+    for (let i = 0; i < n; i++) {
+        let item = ar[i]
+        sum += item
+    }
+    return sum
 }
+
+console.log(simpleArraySum([1,2,3]))
