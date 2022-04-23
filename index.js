55df8beeb5393cc214eac3d8f886d925bc7d82f2
John Doe <rabinarahman@gmail.com>
2022-04-23 04:24:02 -0700

    Remove Arry Item Except Number.But Constrin is Do is Imparative Way

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function aVeryBigSum(ar) {
-    if (ar.length < 0 || ar.length > 10) {
-        return false
+const arr = [null, 2, 3, null, false, 4, "", "test", 5, 6, 7]
+let count = 0
+for (let i = 0; i < arr.length; i++) {
+    // for (let j = i; j < arr.length - 1; j++) {
+    //     if (!arr[j] || typeof arr[j] !== "number") {
+    //         // arr[j] = arr[j + 1]
+    //         // arr[j + 1] = undefined
+
+    //     }
+    // }
+    for (let j =0;j<arr.length;j++){
+        if (!arr[i] || typeof arr[i] !== "number") {
+            arr.splice(i, 1)
+        }
     }
 
-    let sum = 0
-    ar.forEach(num => {
-        if (num < 0 || num > Math.pow(10, 10)) {
-            return false
-        }
-        sum += num
-    })
-    return sum
+    // if (typeof arr[i] === "undefined") {
+    //     count++
+    // }
 }
-
-console.log(aVeryBigSum([0]))
+// arr.length = arr.length - count
+console.log(arr)
