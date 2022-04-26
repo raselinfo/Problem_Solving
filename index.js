80f28de8235c70c25b523f129b7edb4bd0adb872
John Doe <rabinarahman@gmail.com>
2022-04-26 11:06:40 -0700

    Phone Book

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
+const names = [
+    'Ayman',
+    'Abu Rayhan',
+    'Anik',
+    'Elias Emon',
+    'Engr. Sabbir',
+    'Fahim Faisal',
+    'Feroz Khan',
+    'Habib',
+    'HM Azizul',
+    'Hridoy Saha',
+    'Jahid Hassan',
+    'Johir',
+    'Md Al-Amin',
+    'Md Arafatul',
+    'Md Ashraful',
+    'Parvez',
+];
 
-function getDiagonalSum(numbers) {
-    let result = numbers.reduce((acc, curr, index) => {
-        let leftToRight = diagonalSum("leftToRight", index, curr)
-        let rightToLeft = diagonalSum("rightToLeft", index, curr)
-        acc += (leftToRight + rightToLeft)
+// const namesGroup = {
+// 	A: ['Ayman', 'Abu Rayhan', 'Anik'],
+// 	E: ['Elias Emon', 'Engr. Sabbir'],
+// 	F: ['Fahim Faisal', 'Feroz Khan'],
+// };
+
+function getPhoneBook(names) {
+    let result = names.reduce((acc, item) => {
+        if (item[0] in acc) {
+            acc[item[0]].push(item)
+        } else {
+            acc[item[0]] = [item]
+        }
+
         return acc
-    }, 0)
+    }, {})
 
     return result
 }
-
-
-function diagonalSum(type, index, curr) {
-    if (index === 0) {
-        if (type === 'leftToRight') {
-            return curr[0]
-        } else {
-            return curr[2]
-        }
-    } else if (index === 1) {
-        if (type === "leftToRight") {
-            return curr[1]
-        } else {
-            return curr[1]
-        }
-    } else {
-        if (type === "leftToRight") {
-            return curr[2]
-        } else {
-            return curr[0]
-        }
-    }
-}
-
-
-const numbers = [
-    [3, 2, 3],
-    [3, 4, 5],
-    [5, 6, 7]
-];
-
-console.time('time')
-let result = getDiagonalSum(numbers)
-console.timeEnd('time');
-
+console.time("time")
+getPhoneBook(names)
+console.timeEnd("time")
