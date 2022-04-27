72da99bc645b8b94633c438f3e2c19a6c27a47ce
John Doe <rabinarahman@gmail.com>
2022-04-27 12:18:44 -0700

    Object VS Array

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-function aVeryBigSum(ar) {
-    if (ar.length < 0 || ar.length > 10) {
-        return false
-    }
+const uuid = require("./uuid")
 
-    let sum = 0
-    ar.forEach(num => {
-        if (num < 0 || num > Math.pow(10, 10)) {
-            return false
-        }
-        sum += num
-    })
-    return sum
+const students = {
+    '7482484e-e695-4c4d-97b4-f7e9ef06c3bc': {
+        id: '7482484e-e695-4c4d-97b4-f7e9ef06c3bc',
+        name: 'Rasel',
+        email: 'rasel@gmail.cjom'
+    },
+    '09065497-6d33-4ef9-8adb-1dadb5fd98fe': {
+        id: '09065497-6d33-4ef9-8adb-1dadb5fd98fe',
+        name: 'Rabina',
+        email: 'rabina@gmail.cjom'
+    },
+    '6903bbe4-486e-46c7-bc31-54aa596e1a53': {
+        id: '6903bbe4-486e-46c7-bc31-54aa596e1a53',
+        name: 'Sakib',
+        email: 'Sakib@gmail.cjom'
+    }
 }
 
-console.log(aVeryBigSum([0]))
+// const id = '6903bbe4-486e-46c7-bc31-54aa596e1a53'
+// const index = students.findIndex(student => student.id === id)
+// students[index].name = "Sakiba"
+// students.splice(index, 1)
+
+// students.push({
+//     id: uuid(),
+//     name: "New Student",
+//     email: "new@gmail.com"
+// })
+
+students['7482484e-e695-4c4d-97b4-f7e9ef06c34545'] = {
+    id: '7482484e-e695-4c4d-97b4-f7e9ef06c34545',
+    name: "New Students",
+    email: "new@gmail.com"
+}
+
+// [+]Read
+// [+]Update
+// [+]Delete
+// [+]create
+// for (let value of Object.keys(students)){
+//    console.log(value)
+// }
+
+const result = Object.values(students).reduce((acc, student) => {
+    if(student.name[0]==='R'){
+        acc.push(student)
+    }
+    return acc
+}, [])
+console.log(result)
