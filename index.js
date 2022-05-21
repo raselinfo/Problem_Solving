f35972d09e9ac5729365fb25b82e7bfccbf6c3b2
Rasel Hossain <rabinarahman@gmail.com>
2022-05-21 08:19:03 -0700

    HOC Function

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-// myFunc()
-// // Function Statement
-// function myFunc() { }
+// let limit = 100
 
-// // Function Expression
-// myTextFunc()
-// const myTextFunc = function () {
-
-// }
-// myTextFunc()
-// // Fat Arrow Function => function expression
-// myFatFunc()
-// const myFatFunc = () => { 
-
+// function test() {
+//     limit = 50
+//     return limit
 // }
 
-// myFatFunc()
+// console.log(test(3))
+// console.log(limit)
 
-// myFunc()
+// function test(arr, data) {
+//     // let newArr=arr
+//     // newArr.push(data)
+//     // return newArr
+//     console.log("hello")
+// }
+// let arr = [1, 2, 3, 4]
+// console.log(test(arr, 5))
+// console.log(arr)
 
-let limit = 50
-function changeLimit() {
-    limit = 5000
-    return limit
-}
-console.log(changeLimit())
+// let a = 10;
+//     let b = 10
+// function test(a,b) {
+//     // let a=40;
+//     // let b=80
+//     return a + b
+// }
+
+// console.log(test(5, 6))
+
+// const randomSum = (max) => {
+//     const random1 = Math.floor(Math.random() * 5 + 1)
+//     const random2 = Math.floor(Math.random() * 5 + 1)
+
+//     return random1 + random2
+// }
+
+// const randomSub = (max) => {
+//     const random1 = Math.floor(Math.random() * 5 + 1)
+//     const random2 = Math.floor(Math.random() * 5 + 1)
+
+//     return random1 - random2
+// }
+
+
+// const randomMulti = (max) => {
+//     const random1 = Math.floor(Math.random() * 5 + 1)
+//     const random2 = Math.floor(Math.random() * 5 + 1)
+
+//     return random1 * random2
+// }
+
+// const randomDiv = (max) => {
+//     const random1 = Math.floor(Math.random() * 5 + 1)
+//     const random2 = Math.floor(Math.random() * 5 + 1)
+
+//     return random1 / random2
+// }
+
+// function randomCalculation(max, cb) {
+//     const random1 = Math.floor(Math.random() * 5 + 1)
+//     const random2 = Math.floor(Math.random() * 5 + 1)
+//     return cb(random1, random2)
+// }
+
+// const sum = randomCalculation(Math.max(5, 10), function (random1, random2) {
+//     return random1 + random2
+// })
+
+// const sub = randomCalculation(Math.max(6, 7), (random1, random2) =>  random1 - random2)
+
+// const mul = randomCalculation(Math.max(6, 7), (random1, random2) => random1 * random2)
+
+// const div = randomCalculation(Math.max(6, 7), (random1, random2) =>  random1 / random2)
+// console.log(sum)
+
+
