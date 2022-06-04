25aa1d2777b0fce853d8c7acd42a93e4e4ddb5d7
Rasel Hossain <rabinarahman@gmail.com>
2022-06-04 11:03:25 -0700

    Promise Problem

--- index.js
+++ index.js
@@ -15,19 +15,101 @@
 
 
 
-function get(url, cb) {
-    let db = {}
-    cb(db)
-}
+// function get(url, cb) {
+//     let db = {}
+//     cb(db)
+// }
 
-function getUserNameFromComment(db){
-    get("/user?username=rabina",()=>{
-        get("/posts?usserId=309",()=>{
-            get("/comment?postId[0]=3",()=>{
-                get("/commentUser?username=userName[0]",()=>{
-                    console.log("new Commentd User Found")
-                })
-            })
-        })
-    }) 
-}
+// function getUserNameFromComment(db){
+//     get("/user?username=rabina",()=>{
+//         get("/posts?usserId=309",()=>{
+//             get("/comment?postId[0]=3",()=>{
+//                 get("/commentUser?username=userName[0]",()=>{
+//                     console.log("new Commentd User Found")
+//                 })
+//             })
+//         })
+//     }) 
+// }
+
+// const isValid = true
+// const getData = new Promise((resolve, reject) => {
+//     if (isValid) {
+//         resolve("I am Rabina")
+//     } else {
+//         reject("I am Rejected")
+//     }
+// })
+
+// getData.then(value => {
+//     console.log(value)
+// }).catch(err=>{
+//     console.log(err)
+// })
+
+// const getData = (ms) => new Promise((resolve,reject) => setTimeout(() => { reject("i am Rejected") }, ms))
+// getData(4000).then(data => console.log(data)).catch(err => console.log(err))
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(value => {
+//         console.log(value)
+//     }).catch(err => {
+//         console.log(err.message)
+//     })
+
+
+// const getData = new Promise((resolve, reject) => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(response => response.json())
+//         .then(value => {
+//             resolve(value)
+//         }).catch(err => {
+//             reject(err.message)
+//         })
+// })
+// getData.then(value=>console.log(value)).catch(err=>console.log(err))
+
+
+const getData = (url) => new Promise.resolve()
+/**
+ * 1. find user by username
+ * 2. find post by userId
+ * 3. find latest post
+ * 4. find comments by post id
+ * 5. find latest comment
+ * 6. find username of the latest commented user
+ */
+
+/**
+ * /users?username=[username]
+ * /posts?user_id=[user_id]
+ * /comments?post_id=[post_id]
+ * /users?username=[username]
+ */
+// function getUserNameFromComment(username) {
+//     get(`users?username=${username}`, (user) => {
+//         get(`posts?user_id=${user.id}`, (posts) => {
+//             get(`comments?post_id=${posts[0].id}`, (comments) => {
+//                 get(`users?username=${comments[0].username}`, (user) => {
+//                     console.log(user);
+//                 });
+//             });
+//         });
+//     });
+// }
+// getData('/users?username=rasel')
+//     .then(value => {
+//         return getData(' /posts?user_id=5')
+//     })
+//     .then(value=>{
+//         return getData('/comments?post_id=6')
+//     })
+//     .then(value=>{
+//         return getData('/users?username="rasel"')
+//     })
+//     .then(user=>{
+//         console.log(user)
+//     }).catch(err=>{
+//         console.log(err)
+//     })
