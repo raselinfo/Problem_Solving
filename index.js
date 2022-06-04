8b7ae16f20d3d598131d5acbad80d93072b1446b
Rasel Hossain <rabinarahman@gmail.com>
2022-06-04 09:21:42 -0700

    Callback hell

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const toggle = document.querySelector("#toggle");
-const body = document.querySelector("body");
-// let switchMood = "light"
-toggle.addEventListener("click", () => {
-    if (body.className === "dark") {
-        body.className = "light"
-    } else {
-        body.className = "dark"
-    }
-    // console.log(switchMood)
-    // if (switchMood = "light") {
-    //     body.style.backgroundColor = "black"
-    //     switchMood = "dark"
-    //     console.log("I am Dark")
-    // } else {
-    //     body.style.backgroundColor = "white"
-    //     switchMood = "light"
-    //     console.log("I am Light")
-    // }
+/**
+ * 1. find user by username
+ * 2. find post by userId
+ * 3. find latest post
+ * 4. find comments by post id
+ * 5. find latest comment
+ * 6. find username of the latest commented user
+ */
 
-})
+
+// /user?username=username [+]
+//     / posts ? userid = userid[0][+]
+//         / comment ? postId = postId[0]
+//             / commentUser ? username = username
+
+
+
+function get(url, cb) {
+    let db = {}
+    cb(db)
+}
+
+function getUserNameFromComment(db){
+    get("/user?username=rabina",()=>{
+        get("/posts?usserId=309",()=>{
+            get("/comment?postId[0]=3",()=>{
+                get("/commentUser?username=userName[0]",()=>{
+                    console.log("new Commentd User Found")
+                })
+            })
+        })
+    }) 
+}
