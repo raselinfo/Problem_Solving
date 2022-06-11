9e6f40f1ab0953ff9ca724b7ace736b71fa905ec
Rasel Hossain <rabinarahman@gmail.com>
2022-06-11 06:32:26 -0700

    Diagonals Difference

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # [RAselOfficial](https://www.raselofficial.com)
 
 ### Calculate he absolute difference between the sums of its diagonals.
+
+```js
+const processData = (arr) => {
+  const row = arr.length;
+  return Math.abs(
+    arr.reduce((acc, item, index) => {
+      return (acc += parseInt(item[index]) - parseInt(item[row - (index + 1)]));
+    }, 0)
+  );
+};
+const arr = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
+const result = processData(arr);
+console.log(result);
+```
--- index.js
+++ index.js
@@ -1,45 +1,36 @@
+// function processData(input) {
+//     var lines = input.split("\n");
+//     //[ '3', '11 2 4', '4 5 6', '10 8 -12' ]
 
+//     var N = parseInt(lines[0]);
+//     //3
+//     var ans = 0;
 
-
-function diagonalDifference(arr) {
-    let row = 0; let col = 0;
-    let leftToRight = 0;
-    let rightToLeft = 0;
-    let leftRow = 2
-    for (let i = 0; i < arr.length; i++) {
-        const item = arr[i]
-        for (let j = 0; j < item.length; j++) {
-            if (row === i) {
-                if (col === i) {
-                    leftToRight += item[col]
-                    col += 1
-                }
-                row += 1
-            }
-        }
-        for (let k = (item.length - 1); k >= 0; k--) {
-            if (leftRow === k) {
-                rightToLeft += item[k]
-            }
-        }
-        leftRow -= 1
-    }
-    return Math.abs(leftToRight - rightToLeft)
-}
-
-
-// function diagonalDifference(arr) {
-//     const result = arr.reduce((acc, item, index) => {
-//         console.log(index)
-//     }, 0)
+//     for (i = 1; i <= N; i++) {
+//         var line = lines[i].split(" ");
+//         // [ '11', '2', '4' ]
+//         // ['4', '5', '6']
+//         // ['10', '8', '-12']
+//         // console.log(line[i - 1], line[N - i])
+//         // 10-(-12)
+//         const result = (parseInt(line[i - 1]) - parseInt(line[N - i]))
+//         ans = ans + result;
+//     }
+//     // console.log(Math.abs(ans));
 // }
 
-
+const processData = (arr) => {
+    const row = arr.length
+    return Math.abs(arr.reduce((acc, item, index) => {
+        return acc += parseInt(item[index]) - parseInt(item[row - (index + 1)])
+    }, 0))
+}
 const arr = [
     [11, 2, 4],
     [4, 5, 6],
-    [10, 8, 12]
+    [10, 8, -12]
 ]
-
-const result = diagonalDifference(arr)
+const result = processData(arr)
 console.log(result)
+
+
