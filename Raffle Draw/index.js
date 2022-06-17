f8b25b7652ef281c036ff1e9f68e2add94e19e49
Rasel Hossain <rabinarahman@gmail.com>
2022-06-17 16:31:53 -0700

    Raffle Draw

---
+++ Raffle Draw/index.js
@@ -0,0 +1,41 @@
+const db = [
+    {
+        name: "Rasel",
+    }, 
+    {
+        name: "Rabina",
+    },
+     {
+        name: "Sakib",
+    }, 
+    {
+        name: "Santo",
+    }, 
+    {
+        name: "Imran",
+    },
+     {
+        name: "Jakir",
+    }
+]
+
+function draw(winnerCount) {
+    const winner = new Array()
+    const winnerIndexes = []
+    let index = 0
+    while (index < winnerCount) {
+        let indexes = Math.floor(Math.random() * db.length)
+        console.log(indexes)
+        if (!winnerIndexes.includes(indexes)) {
+            winnerIndexes.push(indexes)
+            winner.push(db[indexes])
+            index++
+            continue;
+        }
+    }
+    console.log(winnerIndexes)
+    console.log(winner)
+
+}
+
+draw(2)
